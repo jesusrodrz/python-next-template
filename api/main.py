7e4ba01ef1a376b7018c60c2762e8e54aa6c6baa
jesusrodrz <jesus.rodrz97@gmail.com>
2024-11-27 21:24:31 -0400

    add ternary operator

--- api/main.py
+++ api/main.py
@@ -9,4 +9,4 @@ async def main():
     return {"Hello": "World"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, log_level=Environment.DEBUG or None)
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level= 'debug' if Environment.DEBUG else None)
