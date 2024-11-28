0bc7187a8728785810d494245e1f049b189d505e
jesusrodrz <jesus.rodrz97@gmail.com>
2024-11-27 20:28:47 -0400

    init

--- api/main.py
+++ api/main.py
@@ -1,1 +1,12 @@
+import uvicorn
 from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def main():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="debug")
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { ThemeProvider } from "shared/theme/theme-provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +12,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
-    </html>
+    <ThemeProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ThemeProvider>
   );
 }
